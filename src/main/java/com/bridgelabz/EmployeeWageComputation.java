30429d6fd63e691e51ab610023972ca591cd3147
sandeepvarma8466 <svarma852@gmail.com>
2022-06-13 12:26:33 +0530

    Employee Daily Wage

--- src/main/java/com/bridgelabz/EmployeeWageComputation.java
+++ src/main/java/com/bridgelabz/EmployeeWageComputation.java
@@ -2,12 +2,14 @@ package com.bridgelabz;
 
 public class EmployeeWageComputation {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation");
-        int isPresent = 1;
+        System.out.println("Calculate Daily Employee Wage");
+        int isPresent = 1, empHrs = 8, empWage, empRatePerHr = 20;
         int empCheck = (int) (Math.random() * 10) % 2;
 
         if (empCheck == isPresent) {
             System.out.println("Employee is Present");
+            empWage = empHrs * empRatePerHr;
+            System.out.println(empWage);
         }
         else {
             System.out.println("Employee is Absent");
