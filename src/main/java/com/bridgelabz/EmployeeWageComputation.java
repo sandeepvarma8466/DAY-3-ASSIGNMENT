9fe610b72f9714674dc48754d669d5fa01d7d47b
sandeepvarma8466 <svarma852@gmail.com>
2022-06-13 12:00:49 +0530

    Employee Attendace

--- src/main/java/com/bridgelabz/EmployeeWageComputation.java
+++ src/main/java/com/bridgelabz/EmployeeWageComputation.java
@@ -3,5 +3,14 @@ package com.bridgelabz;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
+        int isPresent = 1;
+        int empCheck = (int) (Math.random() * 10) % 2;
+
+        if (empCheck == isPresent) {
+            System.out.println("Employee is Present");
+        }
+        else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
